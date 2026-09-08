19441b0881ef2ce7e8ba818c88880d55c7f50d5f
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-07 22:46:22 -0700

    Initialize backend course sync before LMS

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,11 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { bootstrapCourseSync } from './courseSync.js'
 
-createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
+async function start() {
+  await bootstrapCourseSync()
+  createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
+}
+
+start()
